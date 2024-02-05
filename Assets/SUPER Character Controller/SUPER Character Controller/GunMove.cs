6f60e26b1640e52abac05ed4d39ca3bba932cbb5
skrinkle dinky <devwinterps@gmail.com>
2024-02-05 13:24:39 -0500

    ff
    ff

--- Assets/SUPER Character Controller/SUPER Character Controller/GunMove.cs
+++ Assets/SUPER Character Controller/SUPER Character Controller/GunMove.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class GunMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float armRotationSpeed = 5f;
+    public float delayBeforeArmsMove = 2f;
+
     void Start()
     {
-        
+        StartCoroutine(MoveArmsWithDelay());
     }
 
-    // Update is called once per frame
+    IEnumerator MoveArmsWithDelay()
+    {
+        yeild return new WaitForSeconds(delayBeforeArmsMove);
+
+        while(true)
+    }
+
     void Update()
     {
-        
+        if (main.camra != null)
+        { 
+        Vector3D
+0        
+        }
+
     }
 }
